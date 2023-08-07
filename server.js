f9c87d950d35f18c6daa9653b5770da477cbd2a6
Dev.Umar <umarraja230@gmail.com>
2023-08-07 12:24:01 +0500

    Add os for get request

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import express from "express";
 import createHttpError from "http-errors";
 import bodyParser from "body-parser";
 import cors from "cors";
+import sys from "os";
 
 // Model Runner
 import ModelRunner from "./Configs/ModelRunner.js";
